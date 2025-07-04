b2cbd60277ae990f685c6b74dc0b861d08579381
Ahmed <md.ahmed701153@gmail.com>
2025-07-04 10:32:11 +0000

    Add JWT server with database integration and authentication

---
+++ users.js
@@ -0,0 +1,35 @@
+import bcrypt from 'bcrypt';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const url = process.env.MONGO_URL;
+const client = new MongoClient(url);
+
+const dbName = "jwt_webserver_users";
+
+
+await client.connect();
+console.log("Client connected!");
+
+const db = client.db(dbName);
+const collection = db.collection('user_data');
+
+const users = collection.find({}).toArray();
+
+const findUser = async (username) => {
+    return users.find(user => user.username == username);
+}
+
+// Registering a User
+const addUser = async (username, password) => {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const pushedData = await collection.insertOne({username, password});
+    const _id = pushedData.insertedId.toString();
+    const newUser = {_id, username, passwordHash};
+    return newUser;
+}
+
+export { users, findUser, addUser };
+
